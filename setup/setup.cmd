778ae9f586c54bca9e5c817d9439ad2710241339
Jannis Gebauer <ja.geb@me.com>
2023-03-06 12:37:39 +0100

    update setup.cmd to not allow keypresses

--- setup/setup.cmd
+++ setup/setup.cmd
@@ -1,6 +1,6 @@
 rem Should be executed from the root project directory
 docker run --rm -v %cd%:/mnt/code python:3.9.7-slim-bullseye python3 -u /mnt/code/setup/env.py
 docker-compose up -d keycloak
-timeout 30
+timeout /T 30 /NOBREAK > nul
 docker-compose run -T app python manage.py shell < setup/keycloak.py
 docker-compose run app python manage.py migrate
