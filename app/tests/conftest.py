eb36dec666888dcd848d19b901c3e40ad0f66c4b
Fukuro192 <salah8elmes.2000@gmail.com>
2023-03-08 13:49:22 +0100

    add test for playlist view

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,3 +1,7 @@
+from tempfile import NamedTemporaryFile
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import io
+import os
 import pytest
 
 from recordings.models import Recording
@@ -7,7 +11,9 @@ from organisations.models import Organisation
 
 @pytest.fixture
 def org():
-    return Organisation.objects.create(name="testorg")
+    organisation = Organisation.objects.create(name="testorg")
+    organisation.create_bucket()
+    return organisation
 
 
 @pytest.fixture
@@ -21,9 +27,31 @@ def user(org):
 
 @pytest.fixture
 def recording(org, user):
-    return Recording.objects.create(
+    black_recording = Recording.objects.create(
         name="recording",
         org=org,
         uploaded_by=user,
         is_uploaded=True
     )
+    with NamedTemporaryFile("rb", delete=False) as file:
+        os.system(
+            # creating a black screen one second video
+            f"ffmpeg -f lavfi -i color=size=1280x720:rate=25:color=black "
+            f"-f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 "
+            f"-t 1 "
+            f"{file.name}.webm 2> /dev/null"
+        )
+        with open(f"{file.name}.webm", "rb") as chunk_file:
+            chunk_io = io.BytesIO(chunk_file.read())
+            file_name = chunk_file.name
+            file_size = os.stat(chunk_file.name).st_size
+        chunk = InMemoryUploadedFile(
+            file=chunk_io,
+            field_name="ChunkField",
+            name=file_name,
+            content_type="webm",
+            size=file_size,
+            charset=None
+        )
+        black_recording.add_chunk(0, chunk)
+    return black_recording
--- app/tests/test_recordings/test_views.py
+++ app/tests/test_recordings/test_views.py
@@ -33,3 +33,21 @@ def test_detail_view(client, recording):
 
     response = client.get(fake_test_url)
     assert response.status_code == 404
+
+
+@pytest.mark.django_db
+def test_playlist_view(client, recording):
+    # uuid of a recording that exists in the database
+    true_uuid = recording.short_id
+    # made up uuid of a non existent recording
+    fake_uuid = str(uuid.uuid4())[:8]
+
+    true_test_url = reverse("recordings:playlist", kwargs={"short_id": true_uuid})
+    fake_test_url = reverse("recordings:playlist", kwargs={"short_id": fake_uuid})
+
+    # no log-in is required
+    response = client.get(true_test_url)
+    assert response.status_code == 200
+
+    response = client.get(fake_test_url)
+    assert response.status_code == 404
