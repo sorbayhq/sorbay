9eaf2eb3218f2c42be1212a1df9641c713ca2c7e
Fukuro192 <salah8elmes.2000@gmail.com>
2023-03-08 11:33:25 +0100

    added device registration test, still incomplete

--- app/tests/test_users/test_views.py
+++ app/tests/test_users/test_views.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 from django.urls import reverse
 
 
@@ -11,3 +12,16 @@ def test_settings_view(client, user):
     client.force_login(user)
     response = client.get(reverse("users:settings"))
     assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_device_register_view(client, user):
+    data = {
+        "token": "",
+        "name": "",
+        "application": "",
+        "release": "",
+    }
+    payload = json.dumps(data)
+    response = client.get(reverse("users:device-register", kwargs={"payload": payload}))
+    assert response.status_code == 302
+    assert reverse("oidc_authentication_init") in response.headers["Location"]
