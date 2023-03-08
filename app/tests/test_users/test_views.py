7a0100869125dc50d1ff50c03dabbf8b63b34e0a
Fukuro192 <salah8elmes.2000@gmail.com>
2023-03-08 11:32:29 +0100

    enhanced on user settings view test

--- app/tests/test_users/test_views.py
+++ app/tests/test_users/test_views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 def test_settings_view(client, user):
     response = client.get(reverse("users:settings"))
     assert response.status_code == 302
+    assert response.headers["Location"] == f"/oidc/authenticate/?next={reverse('users:settings')}"
 
     client.force_login(user)
     response = client.get(reverse("users:settings"))
